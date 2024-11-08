dde5d59e62957bfaca8b69434b654af0054ee756
Lucas2c <00001093590440sp@al.educacao.sp.gov.br>
2024-11-08 11:29:08 -0300

    Add files via upload

---
+++ minha ferramenta de esdudo_lucas matheus/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Netflix',
+    'A Netflix e uma platforma de series e filmes'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasilia'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz bom dia em Inglês?',
+    'Bom dia em ingles é good morning'
+)
